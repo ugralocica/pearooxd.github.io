2ccd514a9f6dc8652edf0edca1af0b89fe60dd66
Elydra5 <kliberbenedek@gmail.com>
2025-12-10 21:02:43 +0100

    fix: subs api structure

--- assets/css/pearoo/index/script.js
+++ assets/css/pearoo/index/script.js
@@ -5,8 +5,9 @@ const subsbar = document.querySelector(".subs-bar");
 (async () => {
     const api = await fetch("https://api.socialcounts.org/youtube-live-subscriber-count/UCsBw4YIB40Zpiw4yVIqxtbg");
     const data = await api.json();
-    const goal = (Math.floor(data['API_sub']/1000)+1)*1000;
-    subs.innerText = `${data['API_sub']}/${goal}`;
-    const barW = (data['API_sub'] * 100) / goal + "%";
+    const subCount = data.counters.estimation.subscriberCount;
+    const goal = (Math.floor(subCount/1000)+1)*1000;
+    subs.innerText = `${subCount}/${goal}`;
+    const barW = (subCount * 100) / goal + "%";
     bar.style.width = barW;
 })();
